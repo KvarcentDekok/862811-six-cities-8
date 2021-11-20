a2cc22637481de72fcbcdc2cba87f5a9120e958f
Kvarcent Dekok <pavhap@gmail.com>
2021-11-20 18:27:30 +0300

    Вносит правки

--- project/src/types/action.ts
+++
@@ -1,7 +0,0 @@
-import { Action } from '@reduxjs/toolkit';
-import { AxiosInstance } from 'axios';
-import { ThunkDispatch } from 'redux-thunk';
-
-import { State } from './state';
-
-export type ThunkAppDispatch = ThunkDispatch<State, AxiosInstance, Action>;
